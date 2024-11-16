a693052c92a2ecc8deb66fc5896eea059ef3c5bd
Arman Chinai <chinaiarman@gmail.com>
2024-11-15 18:35:50 -0800

    Update server.js

--- server/server.js
+++ server/server.js
@@ -14,6 +14,10 @@ const PORT = process.env.PORT || 5000;
 const app = express();
 
 
+// SERVICES
+// pass db to Database class here probably. Still need to make db class.
+
+
 // MIDDLEWARE
 app.use(cors())
 app.use(bodyParser.json())
