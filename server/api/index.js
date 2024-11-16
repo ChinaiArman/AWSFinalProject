ad3da50322cdd3e5227563e64baca36f111911e5
Natalie Yu <nyu17@my.bcit.ca>
2024-11-16 13:40:01 -0800

    Add course routes

--- server/api/index.js
+++ server/api/index.js
@@ -2,6 +2,7 @@
 import express from "express";
 
 import authenticationRoutes from "./authentication.js";
+import Course from "../models/Course.js";
 
 
 // CONSTANTS
@@ -11,6 +12,16 @@ const router = express.Router();
 // ROUTES
 router.use('/auth', authenticationRoutes);
 
+// Course Routes
+router.get('/getAllCourses', async (req, res) => {
+    let courses = await Course.findAll()
+    res.send(courses);
+})
+
+router.post('/createCourse', async (req, res) => {
+    let course = await Course.create(req.body);
+    res.send(course);
+})
 
 // EXPORTS
 export default router;
