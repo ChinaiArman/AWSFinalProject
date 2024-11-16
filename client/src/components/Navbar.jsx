9b13d9d1828a0bd9a89e0296356b38ed67770f47
Ehsan138 <67409692+Ehsan138@users.noreply.github.com>
2024-11-15 21:18:50 -0800

    Add Navbar.jsx, will change later to use s3 for logo

---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Navbar() {
+  return (
+    <div className="bg-blue-500 text-white p-4 flex justify-between">
+      <div>Logo</div>
+      <div>Profile | Home</div>
+    </div>
+  );
+}
+
+export default Navbar;
