e8d6366fd6fac81e387ae3aef6b56cf9430ae881
Victor Fung <98victorfung@gmail.com>
2024-11-30 17:33:43 -0800

    updated secure

--- server/config/sessionConfig.js
+++ server/config/sessionConfig.js
@@ -29,8 +29,7 @@ function createSessionConfig(dbConfig) {
         saveUninitialized: false,
         cookie: {
             httpOnly: true,
-            // secure: process.env.NODE_ENV === 'production',
-            secure: false,
+            secure: process.env.NODE_ENV === 'production',
             maxAge: 1000 * 60 * 60 * 24 * 7
         }
     });
